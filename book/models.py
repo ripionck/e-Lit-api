89f64feac6ae3653c2e17756f9fab7e2e67ed4b7
Ripion Chakma <ripionchakma.421@gmail.com>
2024-02-18 03:18:06 +0600

    updated all models

--- book/models.py
+++ book/models.py
@@ -1,9 +1,24 @@
 from django.db import models
-from author.models import Author
 from publisher.models import Publisher
 from django.utils.text import slugify
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    date_of_death = models.DateField()
+    biography = models.TextField()
+    image = models.ImageField(default='default.jpg', upload_to='author/images/')
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
+
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
@@ -15,11 +30,14 @@ class Book(models.Model):
     cover = models.ImageField(default='default.jpg', upload_to='book/covers/')
     isbn = models.CharField(max_length=13, unique=True)
     edition = models.CharField(max_length = 100)
-    page_count = models.IntegerField(default=0)
+    page_count = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']  # Ordering by creation date descending
 
     def __str__(self):
         return self.title
--- publisher/models.py
+++ publisher/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Publisher(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name= models.CharField(max_length=100, blank=True, null=True)
+    name= models.CharField(max_length=100)
     logo = models.ImageField(default='default.jpg', upload_to='publisher/images/')
     address = models.TextField()
     is_publisher = models.BooleanField(default=False)
