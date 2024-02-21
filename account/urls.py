58e04ea039090d7fb10210023afec882544cd7af
Ripion Chakma <ripionchakma.421@gmail.com>
2024-02-22 01:42:52 +0600

    added user register functionality

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import RegisterView
 
 router = DefaultRouter()
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='auth_register'),
     path('', include(router.urls))
 ]
